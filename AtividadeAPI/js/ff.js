6ac8d4dcb2c08311a56208d2f020dc06e3d95717
luizjrsoares <116985251+luizjrsoares@users.noreply.github.com>
2022-11-30 21:03:36 -0300

    up

---
+++ AtividadeAPI/js/ff.js
@@ -0,0 +1,15 @@
+window.onload = function(){
+    var map;
+
+    function initialize(){
+        var mapProp = {
+            center: new google.maps.LatLng(-11.708180,-40.148478),
+            scrollwheel:false,
+            zoom: 12,
+            mapTypeId:google.maps.MapTypeId.ROADMAP
+        }
+
+        map = new google.map.Map(document.getElementById('mapa'), map)
+    }
+    initialize()
+}
