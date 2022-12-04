51489111e2451fd4bdbc5c5082e0d12d8d897f20
luizjrsoares <116985251+luizjrsoares@users.noreply.github.com>
2022-12-04 20:25:29 -0300

    up

--- AtividadeAPI/script/script.js
+++ AtividadeAPI/script/script.js
@@ -1,7 +1,7 @@
 var map;
 function initMap() {
     var mapOptions = {
-        center: new google.maps.LatLng(-11.709680, -40.149948),
+        center: new google.maps.LatLng(-11.713870, -40.145932),
         zoom: 8,
         mapTypeId: 'roadmap' // roadmap, satellite, hybrid, terrain
         };
@@ -13,24 +13,34 @@ function initMap() {
         url: "img/house.png",
         scaledSize: new google.maps.Size(30, 30),
       };
+    const image2 = {
+        url: "img/fut.png",
+        scaledSize: new google.maps.Size(30, 30),
+      };
 
     var marker = new google.maps.Marker({
         position: new google.maps.LatLng(-11.709680, -40.149948),
         map: map,
         title: 'Minha casa',
         icon: image,
-        animation: google.maps.Animation.DROP
+        animation: google.maps.Animation.BOUNCE
     });
 
-    $.getJSON('./script/pins.json', function(data) { 
-        $.each(data.pins, function(chave, valor) {
-            const point = new google.maps.LatLng(valor.coordenadas.x, valor.coordenadas.y)
-
+    $.getJSON('./script/pins.json', function(response) { 
+        
+        if (response != null || responde != undefined){
+            var dados = response;
+            console.log(dados);
+        }
+        
+        $.each(dados, function(chave, valor) {
+            const point = new google.maps.LatLng(valor.x, valor.y)
             var marker = new google.maps.Marker({
                 position: point,
                 title: valor.title,
                 map: map,
-                icon: image
+                icon: image2,
+                animation: google.maps.Animation.BOUNCE
             });
 
         });
@@ -38,4 +48,5 @@ function initMap() {
     });
     
 }
+
 
